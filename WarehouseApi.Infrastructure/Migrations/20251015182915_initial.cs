7ac57b4e9905c98db9effc1edbba72875c6df3d3
Esthiber <esthiber1@gmail.com>
2025-10-15 14:31:09 -0400

    Se creo migracion 'inicial'.

---
+++ WarehouseApi.Infrastructure/Migrations/20251015182915_initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WarehouseApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    CategoriasId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.CategoriasId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "CategoriasId", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, "Electronicos" },
+                    { 2, "Ropa" },
+                    { 3, "Hogar" },
+                    { 4, "Juguetes" },
+                    { 5, "Deportes" },
+                    { 6, "Libros" },
+                    { 7, "Belleza" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
--- WarehouseApi.Presentation/Program.cs
+++ WarehouseApi.Presentation/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WarehouseApi.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,14 +10,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var ConStr = builder.Configuration.GetConnectionString("MonsterAspDb");
+builder.Services.AddDbContext<Contex>(options =>
+    options.UseSqlServer(ConStr));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
